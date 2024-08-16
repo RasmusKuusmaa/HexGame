1ecc48fcb3f6daf2d6b4ce6f8a3c41de2bc4df37
77f315 <77f315@gmail.com>
2024-08-16 19:55:02 +0300

    show the user the guess they put and what characters were correct

--- src/App.js
+++ src/App.js
@@ -7,39 +7,37 @@ function Test() {
   )
 }
 
-function generateHex(setColor) {
-  const elements = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
-  let hex = "#";
-  for (let i = 0; i < 6; i++) {
-    let item = elements[Math.floor(Math.random() * 16)];
-    hex += item;
-  }
-  setColor(hex);
-}
 
-function GeneratingButton({ setColor }) {
-  return (
-    <button onClick={() => generateHex(setColor)}>
-      Generate new color
-    </button>
-  );
-}
+
 
 function App() {
   const [color, setColor] = useState('#444444');
-  const [chanches, setChanches] = useState(5);
-  useEffect(() => {
-    generateHex(setColor)
-  }, [])
+  const [chances, setChances] = useState(5);
+  
+  const guess = '#dd2345'
+  const splittedGuess = guess.slice(1).split('');
 
   return (
     <div>
       <Test />
       <h1>{color}</h1>
       <div className='boxStyle' style={{ backgroundColor: color }}></div>
-      <GeneratingButton setColor={setColor} />
-      <button onClick={() => setChanches(chanches - 1)}> Guess</button>
-      <h1>Tries left: {chanches}</h1>
+  
+      <button onClick={() => setChances(chances - 1)}> Guess</button>
+      <div className='guessBoxContainer'>
+        <h1>Tries left: {chances}</h1>
+        <div className='guessBox'>
+          {splittedGuess.map((char, index) => (
+            
+            <div key={index} className='guessBoxChar' 
+            id={color[index + 1] === char ? 'correct' : 'incorrect'}
+            >
+              {char}
+            </div>
+          ))}
+        </div>
+      </div>
+      
     </div>
   );
 }

79fb8ba0a130a9ffe2a9dbae3edf07a8783b3f0c
77f315 <77f315@gmail.com>
2024-08-16 19:06:26 +0300

    added a guess button and showed the user how many tries they have left

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+function Test() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>HEX code generator</h1>
+  )
+}
+
+function generateHex(setColor) {
+  const elements = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+  let hex = "#";
+  for (let i = 0; i < 6; i++) {
+    let item = elements[Math.floor(Math.random() * 16)];
+    hex += item;
+  }
+  setColor(hex);
+}
+
+function GeneratingButton({ setColor }) {
+  return (
+    <button onClick={() => generateHex(setColor)}>
+      Generate new color
+    </button>
+  );
+}
+
+function App() {
+  const [color, setColor] = useState('#444444');
+  const [chanches, setChanches] = useState(5);
+  useEffect(() => {
+    generateHex(setColor)
+  }, [])
+
+  return (
+    <div>
+      <Test />
+      <h1>{color}</h1>
+      <div className='boxStyle' style={{ backgroundColor: color }}></div>
+      <GeneratingButton setColor={setColor} />
+      <button onClick={() => setChanches(chanches - 1)}> Guess</button>
+      <h1>Tries left: {chanches}</h1>
     </div>
   );
 }

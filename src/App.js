ec7fac879fbb486ac771c20e434cf58f7be407c4
77f315 <77f315@gmail.com>
2024-08-16 20:09:58 +0300

    changed the guess to an array to make multiple guesses possible

--- src/App.js
+++ src/App.js
@@ -13,9 +13,12 @@ function Test() {
 function App() {
   const [color, setColor] = useState('#444444');
   const [chances, setChances] = useState(5);
-  
-  const guess = '#dd2345'
-  const splittedGuess = guess.slice(1).split('');
+  const [guesses, setGuesses] = useState([]);
+
+  useEffect(() => {
+    setGuesses(['#323099'])
+    setGuesses((prevGuesses) => [...prevGuesses, '#432432'])  
+  }, [])
 
   return (
     <div>
@@ -26,17 +29,25 @@ function App() {
       <button onClick={() => setChances(chances - 1)}> Guess</button>
       <div className='guessBoxContainer'>
         <h1>Tries left: {chances}</h1>
-        <div className='guessBox'>
-          {splittedGuess.map((char, index) => (
-            
-            <div key={index} className='guessBoxChar' 
-            id={color[index + 1] === char ? 'correct' : 'incorrect'}
-            >
-              {char}
+        {guesses.map((guess, guessIndex) => {
+          const splittedGuess = guess.slice(1).split('');
+          return (
+            <div key={guessIndex} className="guessBox">
+              {splittedGuess.map((char, index) => (
+                <div
+                  key={index}
+                  className='guessBoxChar'
+                  id={color.slice(1)[index] === char ? 'correct' : 'incorrect'}
+                >
+                  {char}
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          );
+        })}
       </div>
+   
+      <div className='guessBox'></div>
       
     </div>
   );
